362164d65664b36b15318322ab44f34bfb85bfc3
Nicolás Forlano <nicoforlano@protonmail.com>
2022-03-13 15:11:23 -0300

    Linked list insert and remove methods

--- src/nforla/data-structures/linked_lists/MyLinkedList.java
+++ src/nforla/data-structures/linked_lists/MyLinkedList.java
@@ -24,18 +24,56 @@ public class MyLinkedList {
         this.length++;
     }
 
-    public void insert(int index) {
+    public void insert(Integer insertionIndex,  Object value) {
+        if(insertionIndex == null || insertionIndex < 0) {
+            System.out.println("Can't insert at index null " + insertionIndex);
+            return;
+        }
+        if(insertionIndex == 0) {
+            this.prepend(value);
+            return;
+        }
+        if(insertionIndex >= this.length) {
+            this.append(value);
+            return;
+        }
+        Node leader = this.traverseToIndex(insertionIndex - 1);
+        Node newNode = new Node(value, leader.getNext());
+        leader.setNext(newNode);
     }
 
-    public void remove(int index) {
+    public void remove(Integer removalIndex) {
+        if(removalIndex == null || removalIndex < 0 || removalIndex > this.length) {
+            System.out.println("Can't remove at index " + removalIndex);
+            return;
+        }
+        if(removalIndex == 0) {
+            this.head = this.head.getNext();
+            return;
+        }
+        Node leader = this.traverseToIndex(removalIndex - 1);
+        Node nodeToRemove = leader.getNext();
+        leader.setNext(nodeToRemove.getNext());
     }
 
-    public void traverse() {
+    public Node traverseToIndex(int index) {
+        Node currentNode = this.head;
+        int currentIndex = 0;
+        while(currentIndex != index) {
+            currentNode = currentNode.getNext();
+            currentIndex++;
+        }
+        return currentNode;
+    }
+
+    public void traverseAndPrint() {
+        System.out.println("** LinkedList **");
         Node node = this.head;
         while(node != null) {
-            System.out.println("Node: " + node.getValue());
+            System.out.print("Node: " + node.getValue() + " --> ");
             node = node.getNext();
         }
+        System.out.println("");
     }
 
     public static void main(String[] args) {
@@ -44,6 +82,9 @@ public class MyLinkedList {
         linkedList.append("You");
         linkedList.append(5115);
         linkedList.prepend("WTF");
-        linkedList.traverse();
+        linkedList.insert(3, 99);
+        linkedList.traverseAndPrint();
+        linkedList.remove(3);
+        linkedList.traverseAndPrint();
     }
 }
